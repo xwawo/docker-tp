0395bbdb14471e5bef07c40207d8c2260fd76cb1
xwawo <walidiqbal9@gmail.com>
2023-02-02 16:54:58 +0100

    fix dockerfile backend

--- simple-api/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
+++ simple-api/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
@@ -2,12 +2,10 @@ package fr.takima.training.sampleapplication.IT;
 
 import fr.takima.training.simpleapi.SimpleApiApplication;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.equalTo;
